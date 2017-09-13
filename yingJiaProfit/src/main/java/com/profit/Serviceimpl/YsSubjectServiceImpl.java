6a9834943f13ee0c27329f1074ceb4af324b1e63
Administrator <Administrator@USER-20161004XT>
2017-09-13 17:12:47 +0800

    122

---
+++ yingJiaProfit/src/main/java/com/profit/Serviceimpl/YsSubjectServiceImpl.java
@@ -0,0 +1,36 @@
+package com.profit.Serviceimpl;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.profit.bean.Subject;
+import com.profit.dao.YsSubjectDao;
+import com.profit.service.YsSubjectService;
+@Service
+@Transactional
+public class YsSubjectServiceImpl implements YsSubjectService{
+	@Autowired
+	@Qualifier("ysSubjectDaoImpl")
+	private YsSubjectDao subjectDao;
+	@Override
+	public List<Subject> listSubject() {
+		return subjectDao.listSubject();
+	}
+
+	@Override
+	public Subject getById(int id) {
+		Subject sub=this.subjectDao.getById(id);
+		return sub;
+	}
+
+	@Override
+	public void updateSub(Subject sub) {
+		this.subjectDao.updateSub(sub);
+	}
+
+}
