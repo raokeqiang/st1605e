292144bfca4fb3138d4a90b869bf579cbf92ef75
Administrator <Administrator@USER-20161004XT>
2017-09-13 17:12:09 +0800

    213

---
+++ yingJiaProfit/src/main/java/com/profit/dao/YsSubjectDao.java
@@ -0,0 +1,15 @@
+package com.profit.dao;
+
+import java.util.List;
+
+
+import org.hibernate.Session;
+
+import com.profit.bean.Subject;
+//固守类p2p
+public interface YsSubjectDao{
+   Session getSession();
+	 List<Subject >listSubject();
+	  Subject getById(int id);
+	  void updateSub(Subject sub);
+	}
