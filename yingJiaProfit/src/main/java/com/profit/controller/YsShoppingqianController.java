bdd7a928f2c82ff9e221dbf91c62715e8130a4f6
Administrator <Administrator@USER-20161004XT>
2017-09-28 08:33:55 +0800

    闫9.28

--- yingJiaProfit/src/main/java/com/profit/controller/YsShoppingqianController.java
+++ yingJiaProfit/src/main/java/com/profit/controller/YsShoppingqianController.java
@@ -11,12 +11,10 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.profit.bean.Member;
